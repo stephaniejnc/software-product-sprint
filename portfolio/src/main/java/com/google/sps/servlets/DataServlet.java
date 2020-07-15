d71a176ac1e7d7fd8dafdcd069616a9d186ffe9e
stephanie <stephaniechen0630@gmail.com>
2020-07-14 22:35:07 -0400

    Step 5: loading entities complete and functional

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -19,6 +19,9 @@ import com.google.gson.Gson;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.SortDirection;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -30,11 +33,20 @@ import java.util.ArrayList;
 
 public class DataServlet extends HttpServlet {
 
-  private ArrayList<String> comments = new ArrayList<String>();
-
   // gets data from server
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    
+    ArrayList<String> comments = new ArrayList<String>();
+
+    // load comment entities
+    Query query = new Query("Comment").addSort("timestamp", SortDirection.DESCENDING);
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    PreparedQuery results = datastore.prepare(query);
+    for (Entity entity : results.asIterable()) {
+      String comment = (String) entity.getProperty("comment");
+      comments.add(comment);
+    }
 
     // convert array list to JSON
     String json = convertToJsonUsingGson(comments);
@@ -60,12 +72,14 @@ public class DataServlet extends HttpServlet {
     // this will be executed after the user clicks the "Submit" button
 
     // getting user comment and populating comments data structure
+    long timestamp = System.currentTimeMillis();
     String comment = getUserComment(request);
-    comments.add(comment);
+    // comments.add(comment);
 
     // create new Entity instance of kind Comment
     Entity commentEntity = new Entity("Comment");
     commentEntity.setProperty("comment", comment);
+    commentEntity.setProperty("timestamp", timestamp);
 
     // put entity into Datastore
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
